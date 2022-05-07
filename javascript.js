971ab4b35bb42842f18708a169342e8131410aa1
Aisha <aisyanez.1235@gmail.com>
2022-05-07 13:33:06 +0200

    Menu hamburguesa

---
+++ javascript.js
@@ -0,0 +1,20 @@
+const btn = document.querySelector('#btn-menu');
+const link = document.querySelector('.nav-link');
+const bars = document.querySelector('.fa-bars');
+const times = document.querySelector('.fa-times');
+const nav = document.querySelector('.encabezado');
+
+// const nav_items = document.getElementById('nav-bar');
+const nav_items = document.querySelector('#nav-bar');
+
+
+btn.addEventListener('click', () => {
+    times.classList.toggle('close');
+    bars.classList.toggle('close');
+    nav.classList.toggle('show');
+})
+
+nav_items.addEventListener('click', (e) => {
+    if(e.target.classList == "nav-link")
+        nav.classList.toggle("show");
+})
