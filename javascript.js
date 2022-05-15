39c51352f4f11bca213c348b0fc90a3edbf72d62
AishaYG <aisyanez.1235@gmail.com>
2022-05-15 20:48:09 +0200

    Actualizar

--- javascript.js
+++ javascript.js
@@ -3,9 +3,9 @@ const link = document.querySelector('.nav-link');
 const bars = document.querySelector('.fa-bars');
 const times = document.querySelector('.fa-times');
 const nav = document.querySelector('.encabezado');
-
 // const nav_items = document.getElementById('nav-bar');
 const nav_items = document.querySelector('#nav-bar');
+const btn_up = document.querySelector('#btn-scroll');
 
 
 btn.addEventListener('click', () => {
@@ -18,3 +18,5 @@ nav_items.addEventListener('click', (e) => {
     if(e.target.classList == "nav-link")
         nav.classList.toggle("show");
 })
+
+btn_up.addEventListener('click', () => window.scrollTo(0,0))
